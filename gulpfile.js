955ec8b011fe9dbe5acd4e271ce60450f1a2a13e
rico-fundmore <rico@fundmore.ai>
2019-11-13 10:03:51 -0500

    add 4 helper modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 gulp.task('sass', function(){
  return gulp
   .src('scss/**/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('css'))
+  .pipe(postcss(plugins))
+  .pipe(gulp.dest('./css/min'))
+  .pipe(browserSync.stream())
+})
+
+gulp.task('default', function(){
+  browserSync.init({ server: './' })
+  gulp.watch('scss/**/*.scss', gulp.series('sass'))
+  gulp.watch('*.html').on('change', browserSync.reload)
 })
